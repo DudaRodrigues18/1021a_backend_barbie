c1a7a0984d4194cea1737577e1377b1c508ece54
Maria Eduarda Rodrigues <maria.rodrigues18@estudante.ifms.edu.br>
2023-10-25 07:45:33 -0400

    passando no teste filmes

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,17 @@ import express from 'express';
 const app = express();
 
 // Define uma rota padrão
-app.get('/filmes', (req, res) => {
-    res.send(201);
+app.get('/', (req, res) => {
+    res.send('Olá, mundo!');
+});
+
+app.post('/filmes', (req, res) => {
+    const filme = {
+        titulo: 'Vingadores',
+        descricao: 'Filme dos Vingadores',
+        foto: 'https://live.staticflickr.com/7270/6976087418_59719341f5_b.jpg'
+    }
+    res.status(201).send(filme)
 });
 
 // Inicia o servidor
